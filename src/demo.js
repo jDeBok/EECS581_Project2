5c9fa119de31f66c0563653be2609c3017dfea4a
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-15 13:31:43 -0500

    Update demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,3 +1,28 @@
 UI = UIConstructor();
 
 UI.init();
+async function run() {
+  await UI.receiveMessage({ code: MessageToUICode.ShowTitleScreen });
+}
+
+run();
+
+
+// class GameMaster {
+//   constructor() {
+//     this.ui = new UI();
+//     this.gm = new GameModel();
+//     this.msgToUI = null;
+//     this.msgToGm = null;
+//   }
+// 
+//   init() {
+//     ui.init();
+//     msgToUI = gameModel.init();
+//   }
+// 
+//   run() {
+//     msgToGm = ui.run(msgToUI);
+//     msgToUI = gm.run(msgToGm);
+//   }
+// }
