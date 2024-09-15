d7a06d1d53208000af078db0d06212f94bc95113
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-15 13:30:56 -0500

    Update enums.js

--- src/enums.js
+++ src/enums.js
@@ -1,6 +1,6 @@
 const Player = Object.freeze({
 	P1: 0,
-	P2: 1
+  P2: 1
 });
 
 const Module = Object.freeze({
@@ -27,14 +27,14 @@ const Gamemode = Object.freeze({
 	PlaceShips: 1,
 	MainGame: 2,
 	GameWin: 3,
-	SwitchPlayers: 4,
-	TitleScreen: 5
+  SwitchPlayers: 4,
+  TitleScreen: 5
 });
 
 const MessageToGameModelCode = Object.freeze({
 	Advance: 0,
 	RuleSelect: 1,
-	StartGame: 2,
+  StartGame: 2,
 	PlaceShip: 3,	
 	MakeShot: 4
 });
@@ -42,7 +42,7 @@ const MessageToGameModelCode = Object.freeze({
 const MessageToUICode = Object.freeze({
 	BadShot: -2,
 	BadPlacement: -1,
-	ShowBoard: 0,
+  ShowBoard: 0,
 	ShotResult: 1,
 	PlacementResult: 2,
 	ShowRuleSelect: 3,
@@ -50,10 +50,14 @@ const MessageToUICode = Object.freeze({
 	ShowTitleScreen: 5
 });
 
-//I don't know where this needs to be, so I put it here for now --Alex
-function MessageToGameModelContent(coords, shipToPlace, shipToPlaceIndex, rules) {
-	this.coords = coords;
-	this.shipToPlace = shipToPlace;
-    this.shipToPlaceIndex = shipToPlaceIndex;
-	this.rules = rules;
-}
+const UIEvent = Object.freeze({
+	Nothing:		-1,
+	ButtonClick: 0,
+	SoundToggle: 1,
+	GridClick:   2
+});
+
+const UIButton = Object.freeze({
+	Start: 0
+});
+
