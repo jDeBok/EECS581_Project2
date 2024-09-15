b0894432683fc6daa3a341064c3a52c26c1aa121
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-15 15:51:17 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,11 @@ const Constant = Object.freeze({
   ScreenMargin: 0.05,
   FrameInterval: 50
 });
+
+const Color = Object.freeze({
+  Bg: "#283745",
+  BgStroke: "#161F26",
+  TextBox: "#B4BBCD",
+  TextBoxStroke: "#161F26",
+  TextColor: "#161F26"
+});
