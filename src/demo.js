98fdce086d2bf82687985e178a1f8af8676aa2e5
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-15 22:38:40 -0500

    Update demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,40 +1,12 @@
-/* 
- * Program Name:
- * Demo module
- * 
- * Description:
- * Builds and tests UI
- * 
- * Code sources:
- * Self
- * 
- * Author:
- * Code: Jake Bernard
- * 
- * Creation Date:
- * 2024-09-08
- * 
- */
-
-/* code for testing 
-
-UI = UIConstructor();
-
-UI.init();
-async function run() {
-  await UI.receiveMessage({ code: MessageToUICode.ShowTitleScreen });
-}
-
-run();
-
-
 "use strict"
 
 async function main(game) {
   game.init();
   game.run();
 }
-*/
+
+
+
 
 class GameMaster {
   constructor() {
@@ -50,11 +22,11 @@ class GameMaster {
   }
 
   async run() {
-    msgToGm = ui.run(msgToUI);
-    msgToUI = gm.run(msgToGm);
+    msgToGm = ui.receiveMessage(msgToUI);
+    msgToUI = gm.receiveMessage(msgToGm);
   }
 }
 
 const game = new GameMaster();
 
-await main(game);
+main(game);
