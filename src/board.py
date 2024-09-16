a6bcbc475ff5f34d4ea1c5f8c2949f55b37062a3
jDeBok <113641695+jDeBok@users.noreply.github.com>
2024-09-16 16:39:29 -0500

    Fixed board labels

--- src/board.py
+++ src/board.py
@@ -4,6 +4,15 @@ def create_board():
     return [["~" for _ in range(board_size)] for _ in range(board_size)]  # Create a 10x10 grid filled with "~" representing water.
 
 def print_board(board):
-    print("  " + " ".join(str(i) for i in range(board_size)))  # Print column headers (0 to board_size-1).
+    #print("  " + " ".join(str(i) for i in range(board_size)))  # Print column headers (0 to board_size-1).
+    #Additions by team 9
+    print("   A B C D E F G H I J") #print column headers ( a through j)
+    #end Team 9 additions
     for idx, row in enumerate(board):
-        print(idx, " ".join(row))  # Print each row's index followed by its contents, joined by spaces.
+        #additions
+        if( idx == 9 ):
+            print( "10", " ".join(row))  #Special case for i = 10, to line up row headers
+        else:
+            print( " " + str(idx + 1), " ".join(row))  # Print each row's index followed by its contents, joined by spaces. Index 1 through 10
+        #end additions
+        #print(idx, " ".join(row))  # Print each row's index followed by its contents, joined by spaces.
