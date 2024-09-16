aac6527f353f56bb30ed57abd42771b5651f42e5
JakBern <111832686+JakBern@users.noreply.github.com>
2024-09-15 22:05:53 -0500

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,23 @@
 function UIConstructor() {
-  let cmd, cmdBtn, asciiOut;
+  // init places to output or get input
+  let cmd, cmdBtn, asciiOut, asciiOut2, immed;
+  // needed for tracking state -- the current ship when placing ships
   let currentShip;
+  // grabs all html elements
   function init() { 
+    // the simulated command line
     cmd = document.getElementById("cmd");
+    // the submit button
     cmdBtn = document.getElementById("submitCmd");
+    // the first text output
     asciiOut = document.getElementById("asciiOut");
+    // the second text output
+    asciiOut2 = document.getElementById("asciiOut2");
+    // for immediate text output (eg, you got a hit)
+    immed = document.getElementById("immediate");
   }
+
+
   function receiveMessage(msg) {
     switch (msg.code) {
       case MessageToUICode.ShowTitleScreen:
@@ -29,5 +41,49 @@ function UIConstructor() {
         return MainGameStep(msg);
     }
   }
+
+  
+
+  function drawGridForPlacement(board) {
+    asciiOut.innerHtml = `Place your ships, player ${player + 1}\n`;
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        asciiOut.innerHtml += board[i][j].content === null ? "~" : "s";
+      }
+      asciiOut.innerHtml += "\n";
+    }
+  }
+
+  function drawGridForMainGameCurrentPlayer(board) {
+    asciiOut.innerHtml = `Your board\n`;
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        let outchar = board[i][j].content === null ? "~" : "s";
+        if (outchar == "s") {
+          outchar = board[i][j].content.isAlive ? "s" : "d";
+        }
+        asciiOut.innerHtml += outchar;
+      }
+      asciiOut.innerHtml += "\n";
+    }
+  }
+
+  function drawGridForMainGameTargetPlayer(board) {
+    asciiOut.innerHtml = `Enemy's board\n`;
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        let outchar = board[i][j].isShotAt ? "x" : "~";
+        if (outchar == "x") {
+          outchar = board[i][j].content === null ? "o" : "x";
+        }
+        if (outchar == "x") {
+          outchar = board[i][j].content.isParentDead() ? "d" : "x";
+        }
+        asciiOut.innerHtml += outchar;
+      }
+      asciiOut.innerHtml += "\n";
+    }
+  }
+
   return { init, receiveMessage };
 }
